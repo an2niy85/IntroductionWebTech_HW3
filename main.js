c99cf3bd4aa99f9364dfa721c3fb6f26bb9e1791
an2niy85 <an2niy85@mail.ru>
2023-09-14 11:11:19 +0400

    Add HW3 by Web

---
+++ main.js
@@ -0,0 +1,11 @@
+const name = prompt("Введите ваше имя");
+//const name = "Anton"
+
+function greeting(name) {
+    
+    msg = `Привет, ${name}!`
+    // alert(msg);
+    console.log(msg)
+}
+
+greeting(name);
